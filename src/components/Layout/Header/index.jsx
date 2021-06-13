f2a4d2397c681b1018de93479aae3098af881db3
igncalderon <igui2301@gmail.com>
2021-06-13 19:06:56 -0300

    Navbar Bootstrap

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,20 +1,43 @@
 import React from 'react'
 import './navbar.css'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 const Header = () => {
 
     // Creamos nuestro nav para usarlo en el Layout. Por ahora solo tiene un menu de navegacion,
     // pero podriamos crear tambien un banner con alguna imagen o presentacion, por ejemplo.
 
-    return(
-        <nav className="navbar">
-            <ul className="navbar-container">
-                <li>Home</li>
-                <li>Laptops</li>
-                <li>Accesorios</li>
-            </ul>
-        </nav>
-    )
+    return (
+        <div className="App">
+          <Navbar bg="dark" variant="dark"
+            sticky="top" expand="sm" collapseOnSelect>
+            <Navbar.Brand>
+              <img src="" width="40px" height="40px" />
+              
+            </Navbar.Brand>
+    
+            <Navbar.Toggle className="coloring" />
+            <Navbar.Collapse>
+              <Nav>
+                <NavDropdown title="Products">
+                  <NavDropdown.Item href="#products/tea">GPU</NavDropdown.Item>
+                  <NavDropdown.Item href="#products/coffee">Coffee</NavDropdown.Item>
+                  <NavDropdown.Item href="#products/chocolate">Chocolate</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#products/promo">Promo</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#blog">Blog</Nav.Link>
+                <Nav.Link href="#about-us">About Us</Nav.Link>
+                <Nav.Link href="#contact-us">Contact Us</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+    
+          </Navbar>
+          <div className="content">
+            This is a content.
+          </div>
+        </div>
+        )
 }
 
 export { Header }
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -11,7 +11,6 @@ const Layout = ({children}) => {
     return(
         <>
             <Header />
-          
             {children}
             <Footer />
         </>
